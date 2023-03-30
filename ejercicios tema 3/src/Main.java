80e022dca726ebddc4636ba1b3c40001f8a61c83
El-Saka <99428793+El-Saka@users.noreply.github.com>
2023-03-30 15:37:48 -0300

    Add files via upload

---
+++ ejercicios tema 3/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        String cadena1 = "Hola";
+        String cadena2 = "Mundo";
+
+        String resultado = cadena1 + cadena2;
+
+        System.out.println(cadena1 + "\n" + cadena2 + "\n");
+    }
+
+    }
